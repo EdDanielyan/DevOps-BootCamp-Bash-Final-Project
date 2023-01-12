165211a39a0f8cccf32e3c034aaf8a56fe3a2a55
EdDanielyan <102631130+EdDanielyan@users.noreply.github.com>
2023-01-13 03:35:50 +0400

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,28 +1,43 @@
-#!/bin/curl
+#!/bin/bash
 
 currentVersion="0.0.1"
 
-httpSingleUpload()
-{
-    response=$(curl -A curl --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
-}
+function singleDowload() {
 
-printUploadResponse()
-{
-fileID=$(echo "$response" | cut -d "/" -f 4)
-  cat <<EOF
-Transfer File URL: $response
-EOF
-}
+        response=$(curl --progress-bar https://transfer.sh/$(basename "$2")/$(basename "$1") -o "$3/$1")
 
-singleUpload()
-{
-  filePath=$(echo "$1" | sed s:"~":"$HOME":g)
-  if ! -f "$filePath" ;then { echo "Error: invalid file path"; return 1;}; fi
-  tempFileName=$(echo "$1" | sed "s/.*\///")
-  echo "Uploading $tempFileName"
-  httpSingleUpload "$filePath $tempFileName"
-}
+    }
 
-singleUpload "$1" || exit 1
-printUploadResponse
+    function printDownloadResponse(){
+         singleDowload $1 $2 $3 
+         echo "Success!"  
+    }
+
+    while getopts "d:hv" flag; do
+        case $flag in
+    d) echo "Downloading $4" 
+               printDownloadResponse $4 $3 $2
+              ;;
+    h) echo "Description: Bash tool to transfer files from the command line.
+           Usage:
+             -d  ...
+             -h  Show the help ... 
+             -v  Get the tool version
+           Examples:
+               ./transfer.sh test.txt .. ./transfer.sh -d ./test Mij6ca test.txt";;
+        v) echo "0.0.1";;
+          
+        esac
+    done
+
+
+    
+    if [[ $1 != '-d' && $1 != '-v' &&  $1 != '-h' ]]
+    then
+        for i in "$@"
+            do
+                echo "Uploading $i"
+                response=$(curl --progress-bar --upload-file "$i" https://transfer.sh/$(basename "$i"));
+                echo "Transfer File URL: $response"
+            done
+    fi
